8db69b4fac7c96079ecb220af854ff2c6d587732
Carlos-gmz <20223tn066@utez.edu.mx>
2024-01-08 10:36:53 -0600

    Agregue el archivo de las relaciones JPA

---
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/models/role/Role.java
@@ -0,0 +1,17 @@
+package mx.edu.utez.firstapp.models.role;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Long - long
+}
