0c3e0ba7340dd6e5201378a1841310a1d1765679
Carlos-gmz <20223tn066@utez.edu.mx>
2024-01-08 10:57:28 -0600

    Clase person

--- Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/models/person/Person.java
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/models/person/Person.java
@@ -1,2 +1,43 @@
-package mx.edu.utez.firstapp.models.person;public class Person {
+package mx.edu.utez.firstapp.models.person;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "people")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 40, nullable = false)
+    private String name;
+
+    @Column(length = 40, nullable = false)
+    private String surname;
+
+    @Column(length = 40)
+    private String lastname;
+
+    @Column(columnDefinition = "DATE", nullable = false)
+    private LocalDate birthdate;
+
+    @Column(length = 18, nullable = false, unique = true)
+    private String curp;
+
+    // Este es para ingresar directamente lenguaje SQL
+    @Column(columnDefinition = "TIMESTAMP DEFAULT NOW()", insertable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime creatAdt;
+
+    @Column(columnDefinition = "BOOL DEFAULT true")
+    private boolean status;
+
+
 }
