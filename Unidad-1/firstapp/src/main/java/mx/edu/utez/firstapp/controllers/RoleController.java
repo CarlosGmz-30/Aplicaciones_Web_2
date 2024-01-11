7d533111a960e6e12882c2b7ddd36b5b79b9b80c
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-11 07:58:51 -0600

    Cambios

--- Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/controllers/RoleController.java
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/controllers/RoleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/role")
 @CrossOrigin(origins = {"*"})
-public class RoleController  {
+public class RoleController {
     private final RoleService service;
     public RoleController(RoleService service) {
         this.service = service;
