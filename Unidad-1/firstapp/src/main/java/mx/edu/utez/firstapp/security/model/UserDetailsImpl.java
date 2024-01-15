148231877fd962918d826ad19a71f35fc71a99f0
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-15 12:49:47 -0600

    Cambios generales

--- Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/security/model/UserDetailsImpl.java
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/security/model/UserDetailsImpl.java
@@ -1,4 +1,74 @@
 package mx.edu.utez.firstapp.security.model;
 
-public class UserDetailsImpl {
+import mx.edu.utez.firstapp.models.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+    private final String username;
+    private final String password;
+    private final boolean blocked;
+    private final boolean enable;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(String username, String password, boolean blocked, boolean enable, Collection<? extends GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.blocked = blocked;
+        this.enable = enable;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        Set<GrantedAuthority> authorities = user.getRoles().stream().map(
+                role -> new SimpleGrantedAuthority(role.getName())
+        ).collect(Collectors.toSet());
+        return new UserDetailsImpl(
+                user.getUsername(),
+                user.getPassword(),
+                user.getBlocked(),
+                user.getStatus(),
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return blocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enable;
+    }
 }
