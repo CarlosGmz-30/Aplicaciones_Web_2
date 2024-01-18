5b1ee385acb813469a5201ad9ef514e942807b24
CarlosGmz-30 <20223tn066@utez.edu.mx>
2024-01-18 08:48:05 -0600

    Cambios

--- Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/security/MainSecurity.java
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/security/MainSecurity.java
@@ -1,14 +1,19 @@
 package mx.edu.utez.firstapp.security;
 
+import mx.edu.utez.firstapp.security.jxt.JwtAuthFilter;
 import mx.edu.utez.firstapp.security.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -36,5 +41,22 @@ public class MainSecurity {
         provider.setUserDetailsService(service);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthFilter filter() {
+        return new JwtAuthFilter();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+
     }
 }
