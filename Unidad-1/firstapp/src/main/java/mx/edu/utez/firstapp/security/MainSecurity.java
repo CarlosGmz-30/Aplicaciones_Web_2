5d02db211530dc2d44f255904e0faae61486eb9e
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-22 11:02:50 -0600

    Agregamos cambios en la clase MainSecurity

--- Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/security/MainSecurity.java
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/security/MainSecurity.java
@@ -7,13 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -56,7 +61,18 @@ public class MainSecurity {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(reqs -> reqs
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers("/api/person/**").hasAuthority("ADMIN_ROLE"))
+                .httpBasic(Customizer.withDefaults())
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(filter(), UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> logout.clearAuthentication(true).logoutUrl("/api/auth/logout"));
+        return http.build();
     }
 }
