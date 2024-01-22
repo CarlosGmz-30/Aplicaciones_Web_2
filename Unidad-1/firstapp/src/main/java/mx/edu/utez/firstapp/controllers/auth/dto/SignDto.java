87aad0f32901ffba6e1feaccc52f820ee0484afa
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-22 12:48:58 -0600

    Ni se que hice la vdd

---
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/controllers/auth/dto/SignDto.java
@@ -0,0 +1,11 @@
+package mx.edu.utez.firstapp.controllers.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SignDto {
+    private String username;
+    private String password;
+}
