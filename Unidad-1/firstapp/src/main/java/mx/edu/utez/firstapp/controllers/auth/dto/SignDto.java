40cf759c7f96e0b05b642f7974f3da6ce74cc654
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-25 07:23:25 -0600

    Cambios en controllers

--- Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/controllers/auth/dto/SignDto.java
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/controllers/auth/dto/SignDto.java
@@ -1,5 +1,7 @@
 package mx.edu.utez.firstapp.controllers.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +9,11 @@ import lombok.Setter;
 @Setter
 @Getter
 public class SignDto {
+    @NotBlank
+    @NotEmpty
     private String username;
+
+    @NotBlank
+    @NotEmpty
     private String password;
 }
--- Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/controllers/person/dto/PersonDto.java
+++ Unidad-1/firstapp/src/main/java/mx/edu/utez/firstapp/controllers/person/dto/PersonDto.java
@@ -1,5 +1,7 @@
 package mx.edu.utez.firstapp.controllers.person.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,15 +15,20 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class PersonDto {
     private Long id;
+    @NotEmpty
+    @Size(min = 3, max =40, message = "NameSizeError")
     private String name;
+    @NotEmpty
     private String surname;
     private String lastname;
+    @NotEmpty
     private LocalDate birthDate;
+    @NotEmpty
     private String curp;
     private UserDto user;
 
-    public Person toEntity(){
-        if (user!=null)
+    public Person toEntity() {
+        if (user != null)
             return new Person(name, surname, lastname, birthDate, curp, user.toEntity());
         return new Person(name, surname, lastname, birthDate, curp);
     }
